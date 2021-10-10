685416696a1243e17734375f7299701d42849eb1
Fernando Larios <lariosly2@gmail.com>
2021-10-09 18:27:26 -0600

    Creating single layout to chat

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,54 @@
 import type {NextPage} from 'next'
-import styles from '../styles/Home.module.css'
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Input,
+  InputGroup,
+  Stack,
+} from '@chakra-ui/react'
+import {PhoneIcon, AddIcon, WarningIcon} from '@chakra-ui/icons'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Nerdify</h1>
-      </main>
-    </div>
+    <Box p={10}>
+      <Box boxShadow={`xl`} rounded="md" bg={`gray.100`}>
+        <Flex>
+          <Box w={`30%`} p={6}>
+            <Stack>
+              <InputGroup>
+                <Input bg={`white`} type="text" placeholder="Search" />
+              </InputGroup>
+            </Stack>
+
+            <Box boxShadow={`sm`} minH={200} bg={`white`} mt={5}></Box>
+            <Box boxShadow={`sm`} minH={200} bg={`white`} mt={5}></Box>
+          </Box>
+
+          <Box flex={1} p={6}>
+            <Stack direction="row" spacing={4}>
+              <Button
+                leftIcon={<PhoneIcon />}
+                colorScheme="pink"
+                variant="solid"
+              >
+                Settings
+              </Button>
+              <Button
+                rightIcon={<WarningIcon />}
+                colorScheme="blue"
+                variant="outline"
+              >
+                Call us
+              </Button>
+            </Stack>
+
+            <Box boxShadow={`sm`} minH={430} bg={`white`} mt={5}></Box>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
   )
 }
 

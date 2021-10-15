aaa88726fe1d2baf3bc29ed6cdcb9460fffaf862
GustavoChavarria <gchava92@gmail.com>
2021-10-15 17:28:17 -0600

    Add contact us page (#21)

---
+++ pages/ContactUs/index.js
@@ -0,0 +1,20 @@
+import {Input, VStack, Button} from '@chakra-ui/react'
+
+
+export default function ContactUs() {
+  return (
+
+    <div>
+      <h1>Contact Us</h1>
+
+      <form>
+        <VStack>
+          <Input placeholder="Email" />
+          <Input placeholder="Message" />
+
+
+          <Button>Send Message</Button>
+        </VStack>
+      </form>
+    </div>
+  );

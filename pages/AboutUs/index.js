b49a4e54a1ad9ddc9e92a8305d56f039bdb0c063
Rainer Eli <claushellsing@gmail.com>
2021-10-22 19:04:16 -0600

    Changes improving cserv wording

--- pages/AboutUs/index.js
+++ pages/AboutUs/index.js
@@ -3,8 +3,8 @@ import {Grid, Box, Heading} from '@chakra-ui/react'
 export default function AboutUs() {
   return (
     <div>
-      <h1>Who we are</h1>
-      <h2>We are Nerdify, Based in Nicaragua</h2>
+      <h1>Who we are.</h1>
+      <h2>We are Nerdify, Based in Managua-Nicaragua, Founded in 2006</h2>
 
       <Grid templateColums="repeat(auto-fill, minmax(200px, 1fr))">
         <Box>
--- pages/ContactUs/index.js
+++ pages/ContactUs/index.js
@@ -6,13 +6,12 @@ export default function ContactUs() {
 
     <div>
       <h1>Contact Us</h1>
+      <h2>You can drop us a line at any time (we promise we will back to you in less than 12 hours)</h2>
 
       <form>
         <VStack>
           <Input placeholder="Email" />
           <Input placeholder="Message" />
-
-
           <Button>Send Message</Button>
         </VStack>
       </form>
